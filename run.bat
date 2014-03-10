cdea548c0875f841d734883ae90fccad86c02f06
LeMarwin <Lemarwin42@gmail.com>
2014-03-10 18:03:22 +0400

    Before big work

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-@cd ./bin
+@cd bin
 dmd ../src/main.d ../src/csvparse.d 
 call main.exe
 @cd ..
--- src/main.d
+++ src/main.d
@@ -1,9 +1,21 @@
 module ExplMain;
 
 import std.stdio;
+import CsvParse;
+
+struct Link
+{
+	int from;
+	int to;
+	int value;
+}
 
 int main()
 {
 	writeln("Gotta make homework!");
+	double[][] rawData = getData("testdata.csv");
+	Link data = packData(rawData);
+	foreach(i;rawData)
+		writeln(i); 
 	return 0;
 }
