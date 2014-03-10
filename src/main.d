48a2f7847277b8d76f34ed00212125f09e3bf54b
LeMarwin <Lemarwin42@gmail.com>
2014-03-10 17:33:31 +0400

    first commit

--- src/main.d
+++ src/main.d
@@ -1,9 +1,28 @@
 module ExplMain;
 
 import std.stdio;
+import CsvParse;
+
+struct Link
+{
+	int from;
+	int to;
+	int value;
+}
+
+Link[] packData(double[][] raw)
+{
+	if(raw.length==0)
+		
+	if(raw.length!=raw[0])
+}
 
 int main()
 {
 	writeln("Gotta make homework!");
+	double[][] rawData = getData("testdata.csv");
+	Link[] data = packData(rawData);
+	foreach(i;rawData)
+		writeln(i); 
 	return 0;
 }
